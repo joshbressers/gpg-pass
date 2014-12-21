bb6b161fc41bbc8959deab4de7505036818952e5
bress <josh@bress.net>
2014-12-21 16:55:23 -0600

    Give set_password.py a mail() function

--- gpgpass/set_password.py
+++ gpgpass/set_password.py
@@ -25,109 +25,115 @@ import StringIO
 import getpass
 import optparse
 
-usage = "usage: %prog [options] site"
-parser = optparse.OptionParser(usage=usage)
-parser.add_option("-s", "--store", dest="password_store",
-                  help="Path to the password store", metavar="DIRECTORY")
-parser.add_option("-r", "--recipient", dest="recipient", action="append",
-                  help="Recipient to encrypt the site to. May be used multiple times", metavar="NAME")
+def main():
 
-password_store = None
+    usage = "usage: %prog [options] site"
+    parser = optparse.OptionParser(usage=usage)
+    parser.add_option("-s", "--store", dest="password_store",
+                      help="Path to the password store", metavar="DIRECTORY")
+    parser.add_option("-r", "--recipient", dest="recipient", action="append",
+                      help="Recipient to encrypt the site to. May be used multiple times", metavar="NAME")
 
-(options, args) = parser.parse_args()
+    password_store = None
 
-if len(args) != 1:
-    parser.print_help()
-    sys.exit(1)
+    (options, args) = parser.parse_args()
 
-if os.environ.has_key('GPGPASS_DATABASE'):
-    password_store = os.environ['GPGPASS_DATABASE']
+    if len(args) != 1:
+        parser.print_help()
+        sys.exit(1)
 
-# Let the command line switch override the environment variable
-if options.password_store:
-    password_store = options.password_store
+    if os.environ.has_key('GPGPASS_DATABASE'):
+        password_store = os.environ['GPGPASS_DATABASE']
 
-if password_store is None:
-    parser.print_help()
-    sys.exit(1)
+    # Let the command line switch override the environment variable
+    if options.password_store:
+        password_store = options.password_store
 
-if len(options.recipient) == 0:
-    parser.print_help()
-    sys.exit(1)
+    if password_store is None:
+        parser.print_help()
+        sys.exit(1)
 
-site = args[0]
-new_entry = True
+    if len(options.recipient) == 0:
+        parser.print_help()
+        sys.exit(1)
 
-site_data = {
-                'site'      : None,
-                'username'  : None,
-                'password'  : None
-            }
+    site = args[0]
+    new_entry = True
 
-scrambled_site = hashlib.sha256(site).hexdigest()
+    site_data = {
+                    'site'      : None,
+                    'username'  : None,
+                    'password'  : None
+                }
 
-encrypted_file = os.path.join(password_store, scrambled_site)
+    scrambled_site = hashlib.sha256(site).hexdigest()
 
-# If this file exists, let's update it
-if os.path.isfile(encrypted_file):
-    new_entry = False
+    encrypted_file = os.path.join(password_store, scrambled_site)
 
-gpg_ctx = gpgme.Context()
+    # If this file exists, let's update it
+    if os.path.isfile(encrypted_file):
+        new_entry = False
 
-if new_entry == False:
-    fh = open(encrypted_file, 'r')
-    output = StringIO.StringIO()
-    signatures = gpg_ctx.decrypt_verify(fh, output)
+    gpg_ctx = gpgme.Context()
 
-    output.seek(0)
-    for i in output.readlines():
-        data = i.strip().split(': ')
-        site_data[data[0]] = data[1]
+    if new_entry == False:
+        fh = open(encrypted_file, 'r')
+        output = StringIO.StringIO()
+        signatures = gpg_ctx.decrypt_verify(fh, output)
 
-    fh.close()
+        output.seek(0)
+        for i in output.readlines():
+            data = i.strip().split(': ')
+            site_data[data[0]] = data[1]
 
-    print("Updating %s" % site)
-else:
-    print("Adding %s" % site)
+        fh.close()
 
-if site_data['username'] is None:
-    print("Username: ", end="")
-else:
-    print("Username: [%s] " % site_data['username'], end="")
+        print("Updating %s" % site)
+    else:
+        print("Adding %s" % site)
 
-username = sys.stdin.readline().strip()
-if username == '':
-    username = site_data['username']
+    if site_data['username'] is None:
+        print("Username: ", end="")
+    else:
+        print("Username: [%s] " % site_data['username'], end="")
 
-if site_data['password'] is None:
-    print("Password: ", end="")
-else:
-    print("Password: [return for no change] ", end="")
+    username = sys.stdin.readline().strip()
+    if username == '':
+        username = site_data['username']
 
-# We want a blank password prompt
-password = getpass.getpass("")
-if password == '':
-    password = site_data['password']
+    if site_data['password'] is None:
+        print("Password: ", end="")
+    else:
+        print("Password: [return for no change] ", end="")
 
-site_data['site'] = site
-site_data['username'] = username
-site_data['password'] = password
+    # We want a blank password prompt
+    password = getpass.getpass("")
+    if password == '':
+        password = site_data['password']
 
-plaintext_data = "%s: %s\n%s: %s\n%s: %s" % ('site', site_data['site'],
-                                             'username', site_data['username'],
-                                             'password', site_data['password'])
+    site_data['site'] = site
+    site_data['username'] = username
+    site_data['password'] = password
 
-keys = []
-for i in options.recipient:
-    keys.append(gpg_ctx.get_key(i))
+    plaintext_data = "%s: %s\n%s: %s\n%s: %s" % ('site', site_data['site'],
+                                                 'username', site_data['username'],
+                                                 'password', site_data['password'])
 
-plaintext = StringIO.StringIO(plaintext_data)
-ciphertext = StringIO.StringIO()
-gpg_ctx.armor = True
-gpg_ctx.encrypt(keys, gpgme.ENCRYPT_ALWAYS_TRUST, plaintext, ciphertext)
+    keys = []
+    for i in options.recipient:
+        keys.append(gpg_ctx.get_key(i))
 
-ciphertext.seek(0)
+    plaintext = StringIO.StringIO(plaintext_data)
+    ciphertext = StringIO.StringIO()
+    gpg_ctx.armor = True
+    gpg_ctx.encrypt(keys, gpgme.ENCRYPT_ALWAYS_TRUST, plaintext, ciphertext)
 
-fh = open(encrypted_file, 'w')
-fh.writelines(ciphertext.readlines())
-fh.close
+    ciphertext.seek(0)
+
+    fh = open(encrypted_file, 'w')
+    fh.writelines(ciphertext.readlines())
+    fh.close
+
+if __name__ == "__main__":
+    main()
+
